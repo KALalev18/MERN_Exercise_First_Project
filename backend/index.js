c179362b3c8230e1e83956ebb21d71ca6507f33c
KALalev18 <KALalev18@codingburgas.bg>
2024-08-18 11:33:21 +0300

    Save a new book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,46 @@
-import express from "express"
+import express, { response } from "express"
 import mongoose from 'mongoose';
 import {PORT, mongoDBURL} from "./config.js"
+import { Exercise } from "./models/exerciseModel.js";
 
 const app = express()
+
+// manage post requests
+
+app.use(express.json())
 
 app.get('/', (req, res) =>{
     console.log(req)
     return res.status(234).send('Happy to see MERN responding!')
 })
 
+// Route to save an exercise
 
+app.post('/exercises', async (req, res) =>{
+    try{
+        if(
+            !req.body.title || 
+            !req.body.sets ||
+            !req.body.reps
+        ) {
+            return res.status(400).send({
+                message: "Please fill in all fields!"
+            })
+        }
+        const newExercise = {
+            title: req.body.title,
+            sets: req.body.sets,
+            reps: req.body.reps,
+        }
+        
+        const exercise = await Exercise.create(newExercise)
+
+        return res.status(201).send(exercise)
+    }catch(err){
+        console.log(err.message)
+        response.status(500).send({message: error.message})
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
